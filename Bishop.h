3eb268834fb3e3aeb44e31d5adf2cc337adc725b
yliyli <77973600+yliyli@users.noreply.github.com>
2022-07-24 16:16:21 -0400

    Update Bishop.h

--- Bishop.h
+++ Bishop.h
@@ -9,19 +9,11 @@ namespace Chess
 
   public:
     virtual bool legal_move_shape(std::pair<char, char> start, std::pair<char, char> end) const;
-
-
-    /////////////////////////////////////
-    // DO NOT MODIFY THIS FUNCTION!!!! //
-    /////////////////////////////////////
     char to_ascii() const {
       return is_white() ? 'B' : 'b';
     }
 
   private:
-    /////////////////////////////////////
-    // DO NOT MODIFY THIS FUNCTION!!!! //
-    /////////////////////////////////////
     Bishop(bool is_white) : Piece(is_white) {}
 
     friend Piece* create_piece(char piece_designator);
