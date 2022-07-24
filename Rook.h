05f83e9b481da210eff4722e26e4c72f286fd328
yliyli <77973600+yliyli@users.noreply.github.com>
2022-07-24 17:34:27 -0400

    Update Rook.h

--- Rook.h
+++ Rook.h
@@ -9,18 +9,12 @@ namespace Chess
 
   public:
     bool legal_move_shape(std::pair<char, char> start, std::pair<char, char> end) const ;
-
-    /////////////////////////////////////
-    // DO NOT MODIFY THIS FUNCTION!!!! //
-    /////////////////////////////////////
     char to_ascii() const {
       return is_white() ? 'R' : 'r';
     }
 
   private:
-    /////////////////////////////////////
-    // DO NOT MODIFY THIS FUNCTION!!!! //
-    /////////////////////////////////////
+
     Rook(bool is_white) : Piece(is_white) {}
 
     friend Piece* create_piece(char piece_designator);
